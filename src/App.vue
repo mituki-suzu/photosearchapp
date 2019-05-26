810ae7cde4d2c0dc7c115abd07abcd042e1c42be
mituki-suzu <s.0x0.n_1209@ezweb.ne.jp>
2019-05-26 23:14:14 +0900

    create new file.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+    <myheader></myheader>
+    <p v-if="msg.length > 0">
+        {{msg}}
+    </p>
+    <p v-else>
+        no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="clear()">clear</button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myheader from './components/myheader'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    components: {
+        myheader
+    },
+    data () {
+        return {
+            msg: 'Hello World!!'
+        }
+    },
+    methods: {
+        clear () {
+            this.msg = ''
+        }
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/myheader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="head">
+        写真検索Webアプリケーション
+        <p id="subhead">
+            ～F値・レンズの焦点距離で検索できるアプリケーション～
+        </p>
+    </div>
+</template>
+
+<style>
+#head{
+    font-size:1.5rem;
+    font-weight: bold;
+    /* background-color: aqua; */
+    border-bottom: solid 1px black;
+}
+#subhead{
+    font-size:1rem;
+    margin: 0;
+    color: rgb(138, 138, 138);
+}
+</style>
