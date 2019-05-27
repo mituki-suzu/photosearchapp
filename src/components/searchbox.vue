4623a19f507fffa0674e488fe1cad2e263df57ad
mituki-suzu <s.0x0.n_1209@ezweb.ne.jp>
2019-05-27 11:36:43 +0900

    change file.

--- src/components/searchbox.vue
+++ src/components/searchbox.vue
@@ -1,11 +1,29 @@
 <template>
     <div id="searchbox">
         <input class="search" type="text" placeholder="検索したいワードを入力してください" v-model="msg">
+        {{msg}}
         <div> 
             F値：
             <input type="range" value="1" min="0" max="10" step="0.1" v-model="num">
             {{num}}
         </div>
+        
+        <div class="focal-length">
+            <div v-for="(image,id) in images" :key="image.id" class="focal-length-radio">
+            <label>
+                <!-- 画像選択のラジオボタン -->
+                <input type="radio" name="radio_image" :value="id" v-model="imgId" style="display:none"><br>
+                <p v-if="image.id==imgId">
+                    <img :src="image.imgSelect" class="focal-length-radio-img">
+                </p>
+                <p v-else>
+                    <img :src="image.imgNonSelect" class="focal-length-radio-img">
+                </p>
+            </label>
+        </div>
+
+        </div>
+        
         <button class="search-button" @click="search()">検索</button>
     </div>
 </template>
@@ -14,7 +32,14 @@
 export default {
     data () {
         return {
+            msg: "",
             num: '0',
+            images: [
+                {id: 0, imgSelect: require('@/assets/30mmSelect.png'), imgNonSelect: require('@/assets/30mmNonSelect.png')},
+                {id: 1, imgSelect: require('@/assets/50mmSelect.png'), imgNonSelect: require('@/assets/50mmNonSelect.png')},
+                {id: 2, imgSelect: require('@/assets/75mmSelect.png'), imgNonSelect: require('@/assets/75mmNonSelect.png')},
+            ],
+            imgId: 0,
         }
     },
     methods: {
@@ -38,5 +63,22 @@ export default {
 
 .search-button{
     margin: auto;
+}
+
+.focal-length div:first-of-type{
+    /* クラス内一番最初のdiv要素に付加 */
+    margin-left: 10%;
+}
+
+.focal-length-radio{
+    width:20%;
+    display: inline-block;
+    margin-right: 10%;
+}
+
+
+
+.focal-length-radio-img{
+    width:100%;
 }
 </style>
--- src/components/showresult.vue
+++ src/components/showresult.vue
@@ -15,7 +15,7 @@ export default {
             { id: 1, title: 'My journey with Vue' },
             { id: 2, title: 'Blogging with Vue' },
             { id: 3, title: 'Why Vue is so fun' }
-        ]
+            ],
         }
     },
 }
