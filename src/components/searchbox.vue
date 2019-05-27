a9d8df57822bb87b9613e80e570937da8a3e6592
mituki-suzu <s.0x0.n_1209@ezweb.ne.jp>
2019-05-27 22:15:18 +0900

    change file.

--- src/components/searchbox.vue
+++ src/components/searchbox.vue
@@ -1,10 +1,10 @@
 <template>
     <div id="searchbox">
-        <input class="search" type="text" placeholder="検索したいワードを入力してください" v-model="msg">
-        {{msg}}
+        <!-- <input class="search" type="text" placeholder="検索したいワードを入力してください" v-model="msg">
+        {{msg}} -->
         <div> 
             F値：
-            <input type="range" value="1" min="0" max="10" step="0.1" v-model="num">
+            <input type="range" value="1" min="0" max="10" step="0.1" v-model="num" style="margin-top:50px">
             {{num}}
         </div>
         
@@ -12,11 +12,11 @@
             <div v-for="(image,id) in images" :key="image.id" class="focal-length-radio">
             <label>
                 <!-- 画像選択のラジオボタン -->
-                <input type="radio" name="radio_image" :value="id" v-model="imgId" style="display:none"><br>
-                <p v-if="image.id==imgId">
+                <input type="radio" name="radio_image" :value="id" v-model="imgId" style="display:none; margin:0"><br>
+                <p v-if="image.id==imgId" style="margin:0">
                     <img :src="image.imgSelect" class="focal-length-radio-img">
                 </p>
-                <p v-else>
+                <p v-else  style="margin:0">
                     <img :src="image.imgNonSelect" class="focal-length-radio-img">
                 </p>
             </label>
@@ -24,7 +24,10 @@
 
         </div>
         
-        <button class="search-button" @click="search()">検索</button>
+        <button class="search-button" @click="search(num, imgId)" >検索</button>
+        <p>
+            {{result}}
+        </p>
     </div>
 </template>
 
@@ -32,7 +35,7 @@
 export default {
     data () {
         return {
-            msg: "",
+            // msg: "",
             num: '0',
             images: [
                 {id: 0, imgSelect: require('@/assets/30mmSelect.png'), imgNonSelect: require('@/assets/30mmNonSelect.png')},
@@ -40,12 +43,13 @@ export default {
                 {id: 2, imgSelect: require('@/assets/75mmSelect.png'), imgNonSelect: require('@/assets/75mmNonSelect.png')},
             ],
             imgId: 0,
+            result: "result",
         }
     },
     methods: {
-        search () {
+        search (num, imgID) {
             // flicker apiで検索して結果を返す
-            this.msg = ''
+            this.result = num + " : " + imgID
         },
     },
 }
@@ -53,7 +57,7 @@ export default {
 
 <style>
 #searchbox{
-    border: double 5px blue;
+    /* border: double 5px blue; */
     text-align: center;
 }
 
@@ -65,9 +69,14 @@ export default {
     margin: auto;
 }
 
-.focal-length div:first-of-type{
-    /* クラス内一番最初のdiv要素に付加 */
+/* クラス内一番最初のdiv要素に付加 */
+    /* 親コンポーネントでマージン取る方向に転換 */
+/* .focal-length div:first-of-type{
     margin-left: 10%;
+} */
+
+.focal-length{
+    margin: 0 10%;
 }
 
 .focal-length-radio{
@@ -76,7 +85,9 @@ export default {
     margin-right: 10%;
 }
 
-
+.focal-length-radio:last-child{
+    margin-right: 0;
+}
 
 .focal-length-radio-img{
     width:100%;
--- src/components/showresult.vue
+++ src/components/showresult.vue
@@ -1,9 +1,13 @@
 <template>
 <!-- このコンポーネントでは検索結果を表示する -->
-    <div id="showresult">
-        <p class="result" v-for="(post, id) in posts" :key="post.id">
-            {{id}}.{{post.title}}
-        </p>
+    
+    <div>
+        <p style="text-align:center;">今選択している画像のF値：{{result}}</p>
+        <div id="showresult">
+            <p class="result" v-for="(image) in images" :key="image.id">
+                <img :src="image.img" :alt="image.alt" @mouseover="show(image.alt)">
+            </p>
+        </div> 
     </div>
 </template>
 
@@ -11,25 +15,48 @@
 export default {
     data() {
         return{
-            posts: [
-            { id: 1, title: 'My journey with Vue' },
-            { id: 2, title: 'Blogging with Vue' },
-            { id: 3, title: 'Why Vue is so fun' }
+            result:"test",
+            images: [
+                {id: 0, img: require('@/assets/exsampleImages/im11.jpg'), alt:"f1.1"},
+                {id: 1, img: require('@/assets/exsampleImages/im12.jpg'), alt:"f1.2"},
+                {id: 2, img: require('@/assets/exsampleImages/im13.jpg'), alt:"f1.3"},
+                {id: 3, img: require('@/assets/exsampleImages/im14.jpg'), alt:"f1.4"},
+                {id: 4, img: require('@/assets/exsampleImages/im15.jpg'), alt:"f1.5"},
+                {id: 5, img: require('@/assets/exsampleImages/im16.jpg'), alt:"f1.6"},
+                {id: 6, img: require('@/assets/exsampleImages/im17.jpg'), alt:"f1.7"},
+                {id: 7, img: require('@/assets/exsampleImages/im18.jpg'), alt:"f1.8"},
             ],
         }
+    },
+    methods: {
+        show(alt) {
+            // flicker apiで検索して結果を返す
+            this.result = alt
+        },
     },
 }
 </script>
 
 <style>
 #showresult{
+    width: 80%;
+    margin: 0 10%;
 }
 
 .result{
-    width: 30vw;
-    height: 45vw;
-    border: solid 1px red;
-    float: left;
-    margin-right: 2vw;
+    width: 20%;
+    /* border: solid 1px red; */
+    display: inline-block;
+    margin-right: 6%;
+}
+
+.result:nth-child(4n){
+    margin-right: 0;
+}
+
+.result img{
+    width: 15vw;
+    height: 10vw;
+    object-fit: cover;
 }
 </style>
