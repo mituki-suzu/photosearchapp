084cceb760a93a0a1b209a81d476ef00eabf3e7a
mituki-suzu <s.0x0.n_1209@ezweb.ne.jp>
2019-05-31 17:56:49 +0900

    change file.

--- src/components/searchbox.vue
+++ src/components/searchbox.vue
@@ -28,8 +28,8 @@
         <div class="ac_menu">
             <input type="checkbox" id="Panel1" class="on-off" />
             <label for="Panel1">絞り値</label>
-            <div>
-                F値 : {{num}}
+            <div id="frange">
+                <p id="focalnum">F値 : {{num}}</p>
                 <div> 
                     <input type="range" value="1" min="0" max="10" step="0.1" v-model="num">
                 </div>
@@ -38,45 +38,51 @@
         <div class="ac_menu">
             <input type="checkbox" id="Panel2" class="on-off" />
             <label for="Panel2">焦点距離</label>
-            <div>
+            <div id='focusbase'>
                 <select id="focus" size="1" name="sample">
-                    <option value="14mm">14mm</option>
-                    <option value="15mm">15mm</option>
-                    <option value="17mm">17mm</option>
-                    <option value="20mm">20mm</option>
-                    <option value="22mm">22mm</option>
-                    <option value="24mm">24mm</option>
-                    <option value="28mm">28mm</option>
-                    <option value="32mm">32mm</option>
-                    <option value="35mm">35mm</option>
-                    <option value="40mm">40mm</option>
-                    <option value="45mm">45mm</option>
-                    <option value="50mm">50mm</option>
-                    <option value="60mm">60mm</option>
-                    <option value="65mm">65mm</option>
-                    <option value="70mm">70mm</option>
-                    <option value="85mm">85mm</option>
-                    <option value="90mm">90mm</option>
-                    <option value="105mm">105mm</option>
-                    <option value="135mm">135mm</option>
-                    <option value="180mm">180mm</option>
-                    <option value="200mm">200mm</option>
-                    <option value="300mm">300mm</option>
-                    <option value="400mm">400mm</option>
-                    <option value="500mm">500mm</option>
-                    <option value="600mm">600mm</option>
-                    <option value="800mm">800mm</option>
+                    <option value="">▽　指定なし</option>
+                    <option value="14mm">14</option>
+                    <option value="15mm">15</option>
+                    <option value="17mm">17</option>
+                    <option value="20mm">20</option>
+                    <option value="22mm">22</option>
+                    <option value="24mm">24</option>
+                    <option value="28mm">28</option>
+                    <option value="32mm">32</option>
+                    <option value="35mm">35</option>
+                    <option value="40mm">40</option>
+                    <option value="45mm">45</option>
+                    <option value="50mm">50</option>
+                    <option value="60mm">60</option>
+                    <option value="65mm">65</option>
+                    <option value="70mm">70</option>
+                    <option value="85mm">85</option>
+                    <option value="90mm">90</option>
+                    <option value="105mm">105</option>
+                    <option value="135mm">135</option>
+                    <option value="180mm">180</option>
+                    <option value="200mm">200</option>
+                    <option value="300mm">300</option>
+                    <option value="400mm">400</option>
+                    <option value="500mm">500</option>
+                    <option value="600mm">600</option>
+                    <option value="800mm">800</option>
                 </select>
+                <p>mm</p>
             </div>
         </div>
         <div class="ac_menu">
             <input type="checkbox" id="Panel3" class="on-off" />
             <label for="Panel3">カテゴリ</label>
-            <div>
-                F値 : {{num}}
-                <div> 
-                    <input type="range" value="1" min="0" max="10" step="0.1" v-model="num">
-                </div>
+            <div id="check">
+                <p>
+                <input id="box1" type="checkbox" value="1"/>
+                    風景<br>
+                <input id="box1" type="checkbox" value="2"/>
+                    人物<br>
+                <input id="box1" type="checkbox" value="2"/>
+                    ポートレート<br>
+                </p>
             </div>
         </div>
 
@@ -116,59 +122,53 @@ export default {
 <style>
 #title{
     color: white;
-    font-size: 1.8vw;
-    padding: 5% 18% 0 18%;
-    margin-bottom: 0;
+    font-size: 18px;
+    margin: 34px 0 0 45px;
     font-family: 'Impact';
 }
 
 #subtitle{
     color: white;
-    font-size: 0.7vw;
-    padding: 0% 18%;
-    margin-top: 5px;
+    font-size: 7px;
+    margin: 9px 0 30px 45px;
     font-family: 'Pro"Hiragino Kaku Gothic ProW4'
 }
 
 input[type=range] {
     -webkit-appearance: none;
-    margin: 0;
-    width: 80%;
-    background: #8bc3f3;
+    margin: 33px 0 0 45px;
+    width: 175px;
+    background: #393a45;
 }
+
 input[type=range]:focus {
     outline: none;
 }
 
 /* WebKit・Blinkの溝のスタイル */
 input[type=range]::-webkit-slider-runnable-track {
-  width: 100%;
-  height: 6px;
-  cursor: pointer;
-  background: #66a4f5;
-  border-radius: 5px;
+    width: 17vw;
+    height: 0.5vw;
+    cursor: pointer;
+    background: linear-gradient(90deg, #19696A, #238F91);
+    border-radius: 5px;
 }
 /* WebKit・Blinkのつまみのスタイル */
 input[type=range]::-webkit-slider-thumb {
-  box-shadow: 1px 1px 1px #1453a7, 0px 0px 1px #1453a7;
-  height: 15px;
-  width: 15px;
-  border-radius: 50%;
-  background: #418df0;
-  cursor: pointer;
-  -webkit-appearance: none;
-  /*つまみの縦位置調整 */
-  margin-top: -5px;
+    box-shadow: 1px 1px 1px #fff, 0px 0px 1px #fff;
+    height: 15px;
+    width: 15px;
+    border-radius: 50%;
+    background: #fff;
+    cursor: pointer;
+    -webkit-appearance: none;
+    /*つまみの縦位置調整 */
+    margin-top: -5px;
 }
 
 /* WebKit・Blinkのfocus時のスタイル */
 input[type=range]:focus::-webkit-slider-runnable-track {
-  background: #66a4f5;
-}
-
-#searchbox{
-    /* border: double 5px blue; */
-    /* text-align: center; */
+    background: linear-gradient(90deg, #19696A, #238F91);
 }
 
 .search-button{
@@ -176,32 +176,41 @@ input[type=range]:focus::-webkit-slider-runnable-track {
     float: right;
 }
 
-/* クラス内一番最初のdiv要素に付加 */
-    /* 親コンポーネントでマージン取る方向に転換 */
-/* .focal-length div:first-of-type{
-    margin-left: 10%;
-} */
-
-.focal-length{
-    margin: 0 10%;
+#focalnum{
+    margin: 14px 0 0 45px;
+    color: #fff;
+    font-size: calc(15% + 0.5vw);
 }
 
-/* .focal-length-radio{
-    width:20%;
-    display: inline-block;
-    margin-right: 10%;
+/* 焦点距離 */
+#focusbase p{
+    float: left;
+    font-family: 'Noto Sans CJK JP';
+    font-size: calc(15% + 0.5vw);
+    color: #fff;
+    margin: 18px 0 0 2px; 
 }
 
-.focal-length-radio:last-child{
-    margin-right: 0;
-} */
-
-.focal-length-radio-img{
-    width:100%;
+#focus{
+    float: left;
+    margin: 14px 0 0 45px; 
+    width: 6.5vw;
+    border: solid #393a45 1px;
+    border-bottom: solid #50525D 1px;
+    outline:none;
+    text-indent: 0.01px;
+    text-overflow: '';
+    background: none transparent;
+    vertical-align: middle;
+    font-size: calc(15% + 0.5vw);
+    color: #fff;
+    -webkit-appearance: button;
+    -moz-appearance: button;
+    appearance: button;
 }
 
 .ac_menu{
-  width: 100%;
+    width: 100%;
 }
 
 /* メニューの部分 */
@@ -226,7 +235,6 @@ input[type=range]:focus::-webkit-slider-runnable-track {
     -o-transition: all 0.5s;
     transition: all 0.5s;
     margin: 0;
-    padding: 0 10px;
     list-style: none;
 	background: #2e2e34;
 }
@@ -240,7 +248,7 @@ input[type=range]:focus::-webkit-slider-runnable-track {
 
 .ac_menu label {
 	font-family: 'Noto Sans CJK JP';
-    font-size:  1.0vw;
+    font-size:  10px;
 	line-height: 3;
 	position: relative;
 	display: block;
@@ -264,10 +272,67 @@ input[type=range]:focus::-webkit-slider-runnable-track {
 	transition: all 0.35s;
 	text-align: center;
 }
+
 .ac_menu input[type=checkbox] ~ label::after {
 	content: '▲';
 }
+
 .ac_menu input[type=checkbox]:checked ~ label::after {
 	transform: rotateX(180deg);
 }
+
+#frange{
+    padding: 0;
+}
+
+select option{
+    /* background-color: #fff; */
+    color: #333;
+}
+select::-ms-expand {
+    display: none;
+}
+select:-moz-focusring { 
+    color: transparent; 
+    text-shadow: 0 0 0 #828c9a;
+}
+
+#check {
+    color: #fff;
+}
+
+input[type=checkbox] {
+  background-color: aqua;
+}
+
+/* ゴミ */
+
+/* #searchbox{
+    border: double 5px blue;
+    text-align: center;
+} */
+
+/* クラス内一番最初のdiv要素に付加 */
+    /* 親コンポーネントでマージン取る方向に転換 */
+/* .focal-length div:first-of-type{
+    margin-left: 10%;
+} */
+
+/* .focal-length{
+    margin: 0 10%;
+} */
+
+/* .focal-length-radio{
+    width:20%;
+    display: inline-block;
+    margin-right: 10%;
+}
+
+.focal-length-radio:last-child{
+    margin-right: 0;
+} */
+/* 
+.focal-length-radio-img{
+    width:100%;
+} */
 </style>
--- src/components/showresult.vue
+++ src/components/showresult.vue
@@ -1,21 +1,19 @@
 <template>
 <!-- このコンポーネントでは検索結果を表示する -->
     <div>
-        {{searchValue}}
+        <!-- {{searchValue}} -->
         <div id="showresult">
             <p v-if="loading">
                 Loading...
             </p>
-            <p v-else class="result" v-for="simage in searchimages" :key="simage.id">
-                <img :src="simage.url_n">
+            <p v-else class="result">
+                <img :src="simage.url_n" v-for="simage in searchimages" :key="simage.id">
             </p>  
         </div>
-        <p style="text-align:center;">今選択している画像のF値：{{result}}</p>
-        <div id="showresult">
-            <p class="result" v-for="(image) in images" :key="image.id">
+        <!-- <p style="text-align:center;">今選択している画像のF値：{{result}}</p> -->
+        <!-- <div id="result"  v-for="(image) in images" :key="image.id">
                 <img :src="image.img" :alt="image.alt" @click="show(image.alt)">
-            </p>
-        </div> 
+        </div>  -->
     </div>
 </template>
 
@@ -29,18 +27,18 @@ export default {
     data() {
         return{
             result:"test",
-            images: [
-                {id: 0, img: require('@/assets/exsampleImages/im11.jpg'), alt:"f1.1"},
-                {id: 1, img: require('@/assets/exsampleImages/im12.jpg'), alt:"f1.2"},
-                {id: 2, img: require('@/assets/exsampleImages/im13.jpg'), alt:"f1.3"},
-                {id: 3, img: require('@/assets/exsampleImages/im14.jpg'), alt:"f1.4"},
-                {id: 4, img: require('@/assets/exsampleImages/im15.jpg'), alt:"f1.5"},
-                {id: 5, img: require('@/assets/exsampleImages/im16.jpg'), alt:"f1.6"},
-                {id: 6, img: require('@/assets/exsampleImages/im17.jpg'), alt:"f1.7"},
-                {id: 7, img: require('@/assets/exsampleImages/im18.jpg'), alt:"f1.8"},
-            ],
             loading: false,
             searchimages: [],
+            // images: [
+            //     {id: 0, img: require('@/assets/exsampleImages/im11.jpg'), alt:"f1.1"},
+            //     {id: 1, img: require('@/assets/exsampleImages/im12.jpg'), alt:"f1.2"},
+            //     {id: 2, img: require('@/assets/exsampleImages/im13.jpg'), alt:"f1.3"},
+            //     {id: 3, img: require('@/assets/exsampleImages/im14.jpg'), alt:"f1.4"},
+            //     {id: 4, img: require('@/assets/exsampleImages/im15.jpg'), alt:"f1.5"},
+            //     {id: 5, img: require('@/assets/exsampleImages/im16.jpg'), alt:"f1.6"},
+            //     {id: 6, img: require('@/assets/exsampleImages/im17.jpg'), alt:"f1.7"},
+            //     {id: 7, img: require('@/assets/exsampleImages/im18.jpg'), alt:"f1.8"},
+            // ],
         }
     },
     methods: {
@@ -90,19 +88,24 @@ export default {
 }
 
 .result{
-    width: 20%;
-    /* border: solid 1px red; */
     display: inline-block;
-    margin-right: 6%;
+    line-height: 0;
+ 
+   -webkit-column-count: 4;
+   -webkit-column-gap:   0px;
+   -moz-column-count:    4;
+   -moz-column-gap:      0px;
+   column-count:         4;
+   column-gap:           0px;
 }
 
-.result:nth-child(4n){
-    margin-right: 0;
-}
+
 
 .result img{
-    width: 15vw;
-    height: 10vw;
-    object-fit: cover;
+    /* border: solid 1px red; */
+    width: 80%;
+    height: auto;
+    margin-bottom: 10%;
+    border-radius: 5px;
 }
 </style>
